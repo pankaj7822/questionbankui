a34e6013c07d49a588c8cb6d4bdf9fa9c2fd28a8
pankaj7822 <pankaj.krjha.cse17@itbhu.ac.in>
2020-12-13 20:37:26 +0530

    removed linting errors and Fixed for Production

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,8 +4,10 @@
       <img alt="Vue logo" src="../assets/pcampus_banner.jpg" />
     </div>
     <v-spacer></v-spacer>
-         <a target="_blank" href="https://questionbankioe.herokuapp.com/admin/"> <v-btn color="primary">
-      Admin Login
-    </v-btn></a>
+    <a target="_blank" href="https://questionbankioe.herokuapp.com/admin/">
+      <v-btn color="primary">
+        Admin Login
+      </v-btn></a
+    >
   </v-card>
 </template>
--- src/components/Questiontable.vue
+++ src/components/Questiontable.vue
@@ -3,22 +3,18 @@
     <v-card-title>
       IOE Old Question Database For Electronics and Communication Engineering
       <v-spacer></v-spacer>
-  <v-select
-      v-model="search"
-      :items="courses"
-      label="Search By Course Title"
-    ></v-select>
+      <v-select
+        v-model="search"
+        :items="courses"
+        label="Search By Course Title"
+      ></v-select>
     </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="papers"
-      :search="search"
-    >
-        <template #item.link="{ value }">
-            <a target="_blank" :href="value">
-              {{ value }}
-            </a>
-        </template>
+    <v-data-table :headers="headers" :items="papers" :search="search">
+      <template #item.link="{ value }">
+        <a target="_blank" :href="value">
+          {{ value }}
+        </a>
+      </template>
     </v-data-table>
   </v-card>
 </template>
@@ -28,15 +24,15 @@ export default {
   data() {
     return {
       search: "",
-      courses:[],
+      courses: [],
       headers: [
-        {text: "Course Code", value: "course_code"},
-        {text: "Course Title", value: "course_title"},
-        {text: "Semester", value: "semester"},
-        {text: "Department", value: "department"},
-        {text: "Program", value: "program"},
-        {text: "Course Type", value: "course_type"},
-        {text: "Exam Year", value: "year"},
+        { text: "Course Code", value: "course_code" },
+        { text: "Course Title", value: "course_title" },
+        { text: "Semester", value: "semester" },
+        { text: "Department", value: "department" },
+        { text: "Program", value: "program" },
+        { text: "Course Type", value: "course_type" },
+        { text: "Exam Year", value: "year" },
         { text: "Exam Type", value: "type" },
         { text: "Paper Link", value: "link", sortable: false }
       ],
@@ -46,14 +42,11 @@ export default {
   created() {
     this.$axios
       .get("https://questionbankioe.herokuapp.com/getpapers")
-      .then(response => (this.papers=response.data.paper_list));
+      .then(response => (this.papers = response.data.paper_list));
     this.$axios
-    .get("https://questionbankioe.herokuapp.com/getcourses")
-    .then(response=>(this.courses=response.data.course_list));
+      .get("https://questionbankioe.herokuapp.com/getcourses")
+      .then(response => (this.courses = response.data.course_list));
   },
-  methods:{
-
-  }
-
+  methods: {}
 };
 </script>
